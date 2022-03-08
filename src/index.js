486fad68fce02e19184fa08d68a9dd56fb9deff8
white5moke <chris.is.rad@pm.me>
2022-03-08 05:53:41 -0800

    have to rearrange key exporting, since exportKey() method produces different results based on the key provided. bad design Mozilla.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import {
     Container, Row, Col, Button,
     Card, Table, Image, Spinner,
     Tab, Nav, Tabs, Form, Stack,
-    ButtonGroup
+    ButtonGroup, Text
 } from 'react-bootstrap';
 import crypto from 'crypto';
 
-const CryptoTool = (props) => {
+const RandomStringTool = (props) => {
     const [randomString, setRandomString] = useState();
     const [byteLength, setByteLength] = useState(4);
 
@@ -17,10 +17,11 @@ const CryptoTool = (props) => {
         crypto.randomBytes(byteLength, (err, buf) => {
             setRandomString(buf.toString('hex'))
         });
+
+        
     }, []);
 
     const clickGenRandomString = (e) => {
-        console.log("byte length: " + byteLength);
         crypto.randomBytes(byteLength, (err, buf) => {
             setRandomString(buf.toString('hex'));
         });
@@ -31,36 +32,118 @@ const CryptoTool = (props) => {
     };
 
     return (
-        <Card>
-            <Card.Body>
-                <Card.Title>random string generator</Card.Title>
-                
+        <Stack direction='horizontal' gap={5}>
+            <Card>
+                <Card.Body>
+                    <Card.Title>random string generator</Card.Title>
+                    
+                    <Stack direction='vertical' gap={5}>
+                        <Table striped hover>
+                            <tbody>
+                                <tr>
+                                    <th>byte len.</th>
+                                    <td>{byteLength}</td>
+                                </tr>
+                                <tr>
+                                    <th>random str.</th>
+                                    <td>{randomString}</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label htmlFor='selectByteLength'></Form.Label>
+                                <Form.Select id='selectByteLength' onChange={onSelectBytLength} aria-label="# bytes">
+                                    <option value={4}>4</option>
+                                    <option value={8}>8</option>
+                                    <option value={16}>16</option>
+                                </Form.Select>
+                            </Form.Group>
+                            <Button variant='primary' type='button' onClick={clickGenRandomString}>Random String</Button>
+                        </Form>
+                    </Stack>
+                </Card.Body>
+            </Card>
+        </Stack>
+    );
+};
+
+const SignThings = (props) => {
+    const [signingKey, setSigningKey] = useState({
+        pub: [],
+        priv: []
+    });
+
+    // for exports
+    const [keyFormat, setKeyFormat] = useState('raw');
+
+    useEffect(() => {
+        let keyPair = window.crypto.subtle.generateKey({
+            name: 'ECDSA',
+            namedCurve: 'P-384',
+        }, true, ['sign', 'verify']);
+        keyPair.then((res) => {
+            window.crypto.subtle.exportKey(keyFormat, res.publicKey).then((exported) => {
+                setSigningKey({...signingKey, pub: Buffer.from(exported)});
+            });
+        });
+
+    }, []);
+
+    const selectKeyFormat = (mode) => {
+        setKeyFormat(mode);
+    };
+
+    const clickDoIt = (e) => {
+        if(keyFormat == 'spki') { // public signing keys only
+            window.crypto.subtle.generateKey({
+                name: 'ECDSA',
+                namedCurve: 'P-384',
+            }, true, ['sign', 'verify']).then((res) => {
+                window.crypto.subtle.exportKey('spki', res.publicKey).then((exported) => {
+                    setSigningKey({...signingKey, pub: exported});
+                    let pubStr = String.fromCharCode.apply(null, new Uint8Array(signingKey.pub));
+                    let pubB64 = window.btoa(pubStr);
+                    console.log("-----BEGIN PUBLIC KEY-----\n" + pubB64 + "\n-----END PUBLIC KEY-----");
+
+                });
+            });
+        }
+    };
+
+    return (
+        <Stack direction='vertical' gap={5}>
+            <Card>
+                <Card.Title>signing things</Card.Title>
+                <Card.Subtitle>use this to create and sign strings</Card.Subtitle>
                 <Stack direction='vertical' gap={5}>
                     <Table striped hover>
                         <tbody>
                             <tr>
-                                <th>byte len.</th>
-                                <td>{byteLength}</td>
+                                <th>format</th>
+                                <td>{keyFormat}</td>
                             </tr>
                             <tr>
-                                <th>random str.</th>
-                                <td>{randomString}</td>
+                                <th>public key</th>
+                                <td style={{wordBreak: 'break-all'}}>{signingKey.pub.toString('hex')}</td>
                             </tr>
                         </tbody>
                     </Table>
                     <Form>
                         <Form.Group>
-                            <Form.Select onChange={onSelectBytLength} aria-label="# bytes">
-                                <option value={4}>4</option>
-                                <option value={8}>8</option>
-                                <option value={16}>16</option>
+                            <Form.Label htmlFor='keyFormat'>format?</Form.Label>
+                            <Form.Select onChange={(e) => selectKeyFormat(e.target.value)}>
+                                <option value={'raw'}>raw</option>
+                                <option value={'spki'}>spki</option>
+                                <option value={'pkcs8'}>pkcs8 (good 4 file saving)</option>
+                                <option value={'jwk'}>json web key</option>
                             </Form.Select>
                         </Form.Group>
-                        <Button variant='primary' type='button' onClick={clickGenRandomString}>Random String</Button>
+                        <Button onClick={clickDoIt}>do it</Button>
                     </Form>
                 </Stack>
-            </Card.Body>
-        </Card>
+            </Card>
+        </Stack>
     );
 };
 
@@ -68,8 +151,11 @@ const Main = (props) => {
     return (
         <Container>
             <Row>
-                <Col lg={4}>
-                    <CryptoTool />
+                <Col lg={6}>
+                    <RandomStringTool />
+                </Col>
+                <Col lg={6}>
+                    <SignThings />
                 </Col>
             </Row>
         </Container>
