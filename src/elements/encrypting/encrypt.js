c4a59c8651ad4d33fd668311c1e2943501e18df2
white5moke <chris.is.rad@pm.me>
2022-05-07 09:38:11 -0700

    begin encryption stuff.

--- src/elements/encrypting/encrypt.js
+++ src/elements/encrypting/encrypt.js
@@ -1,8 +1,15 @@
 import React, {useState, useRef, useEffect} from "react";
+import { 
+    ButtonGroup, Stack, Button
+} from 'react-bootstrap';
 
 const Encrypt = () => {
     return (
-        <></>
+        <>
+            <Stack gap={3}>
+                <ButtonGroup></ButtonGroup>
+            </Stack>
+        </>
     );
 };
 
